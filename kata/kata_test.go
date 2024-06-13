53ac59a2bd370c938533d13d8e79245334109bf7
Jacek programmer <jacek301@gmail.com>
2024-06-13 15:02:44 +0200

    Exclusive "or" (xor) Logical Operator kata start

--- kata/kata_test.go
+++ kata/kata_test.go
@@ -1,18 +1,32 @@
+// Ginkgo BDD Testing Framework <http://onsi.github.io/ginkgo></http:>
+// Gomega Matcher Library <http://onsi.github.io/gomega></http:>
+
 package kata_test
 
 import (
+	. "fundamentals/kata"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
-	."fundamentals/kata"
 )
 
-var _ = Describe("Kata", func() {
-	It("Basic Tests", func() {
-		Expect(LoveFunc(1, 4)).To(Equal(true))
-		Expect(LoveFunc(2, 2)).To(Equal(false))
-		Expect(LoveFunc(0, 1)).To(Equal(true))
-		Expect(LoveFunc(0, 0)).To(Equal(false))
+var _ = Describe("Test Example", func() {
+	It("basic tests", func() {
+		Expect(Xor(false, false)).To(Equal(false))
+		Expect(Xor(true, false)).To(Equal(true))
+		Expect(Xor(false, true)).To(Equal(true))
+		Expect(Xor(true, true)).To(Equal(false))
 	})
-
+	It("nested tests", func() {
+		Expect(Xor(false, Xor(false, false))).To(Equal(false))
+		Expect(Xor(Xor(true, false), false)).To(Equal(true))
+		Expect(Xor(Xor(true, true), false)).To(Equal(false))
+		Expect(Xor(true, Xor(true, true))).To(Equal(true))
+		Expect(Xor(Xor(false, false), Xor(false, false))).To(Equal(false))
+		Expect(Xor(Xor(false, false), Xor(false, true))).To(Equal(true))
+		Expect(Xor(Xor(true, false), Xor(false, false))).To(Equal(true))
+		Expect(Xor(Xor(true, false), Xor(true, false))).To(Equal(false))
+		Expect(Xor(Xor(true, true), Xor(true, false))).To(Equal(true))
+		Expect(Xor(Xor(true, Xor(true, true)), Xor(Xor(true, true), false))).To(Equal(true))
+	})
 })
