ec6604b652a3bf41ecaae01c37751d149837f8c7
Jacek programmer <jacek301@gmail.com>
2024-06-13 17:02:44 +0200

    Take the Derivative kata start

--- kata/kata.go
+++ kata/kata.go
@@ -1,11 +1,6 @@
 package kata
 
-func Between(a, b int) []int {
 
-	var numbers []int
-	for number := a; number <= b; number++ {
-		numbers = append(numbers, number)
-	}
-
-	return numbers
+func Derive(coefficient, exponent int) string {
+  return ""
 }
--- kata/kata_test.go
+++ kata/kata_test.go
@@ -7,10 +7,13 @@ import (
   . "fundamentals/kata"
 )
 
+func dotest(c, e int, expected string) {
+     Expect(Derive(c, e)).To(Equal(expected), "With coefficient = %d, exponent = %d", c, e)
+}
 
 var _ = Describe("Tests", func() {
      It("Sample tests", func() {
-       Expect(Between(1, 4)).To(Equal([]int{1, 2, 3, 4}))
-       Expect(Between(-2, 2)).To(Equal([]int{-2, -1, 0, 1, 2}))
+          dotest(7, 8, "56x^7")
+          dotest(5, 9, "45x^8")
      })
 })
