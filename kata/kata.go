045ac51bfd72f31d156750dc01e5ea0370b7671c
Jacek programmer <jacek301@gmail.com>
2024-07-29 17:09:20 +0200

    Grasshopper - Terminal game combat function kata start

--- kata/kata.go
+++ kata/kata.go
@@ -1,14 +1,6 @@
 package kata
 
-import "strconv"
+func combat(health, damage float64) float64 {
 
-func BinToDec(bin string) int {
-
-	deciaml, err := strconv.ParseInt(bin, 2, 64)
-
-	if err != nil {
-		return -1
-	}
-
-	return int(deciaml)
+	return 0
 }
--- kata/kata_test.go
+++ kata/kata_test.go
@@ -1,23 +1,15 @@
-// TODO: replace with your own tests (TDD). An example to get you started is included below.
-// Ginkgo BDD Testing Framework <http://onsi.github.io/ginkgo/>
-// Gomega Matcher Library <http://onsi.github.io/gomega/>
-
-
-package kata_test
+package kata
 import (
   . "github.com/onsi/ginkgo/v2"
   . "github.com/onsi/gomega"
-  . "fundamentals/kata"
 )
-
-
-var _ = Describe("Tests", func() {
-     It("Sample tests", func() {
-       Expect(BinToDec("0")).To(Equal(0))
-       Expect(BinToDec("1")).To(Equal(1))
-       Expect(BinToDec("10")).To(Equal(2))
-       Expect(BinToDec("11")).To(Equal(3))
-       Expect(BinToDec("101010")).To(Equal(42))
-       Expect(BinToDec("1001001")).To(Equal(73))
-     })
+var _ = Describe("Damaged but alive", func() {
+   It("Should return the correct positive value", func() {
+     Expect(combat(100.0, 50.0)).To(Equal(50.0))
+   })
+})
+var _ = Describe("Dead", func() {
+   It("Should return 0 rather than negative health", func() {
+     Expect(combat(1.0, 100)).To(Equal(0.0))
+   })
 })
