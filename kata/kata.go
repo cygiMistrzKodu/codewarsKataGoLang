653ea5f3cb6d2c2df6ab09465d13ccdaec4f207d
Jacek programmer <jacek301@gmail.com>
2024-07-29 17:05:14 +0200

    Bin to Decimal kata start and  solution

--- kata/kata.go
+++ kata/kata.go
@@ -1,29 +1,14 @@
 package kata
 
-import (
-	"math/big"
-)
+import "strconv"
 
-func AmIWilson(n int) bool {
+func BinToDec(bin string) int {
 
-	if n <= 1 {
-		return false
+	deciaml, err := strconv.ParseInt(bin, 2, 64)
+
+	if err != nil {
+		return -1
 	}
 
-	factorialResult := new(big.Int)
-
-	quotient := new(big.Int)
-	remainder := new(big.Int)
-
-	_, remainder = quotient.DivMod(factorialResult.Add(factorial(n-1), big.NewInt(1)), big.NewInt(int64(n*n)), remainder)
-
-	return remainder.Cmp(big.NewInt(0)) == 0
-}
-
-func factorial(n int) *big.Int {
-	result := big.NewInt(1)
-	for i := 1; i <= n; i++ {
-		result.Mul(result, big.NewInt(int64(i)))
-	}
-	return result
+	return int(deciaml)
 }
--- kata/kata_test.go
+++ kata/kata_test.go
@@ -1,23 +1,23 @@
+// TODO: replace with your own tests (TDD). An example to get you started is included below.
+// Ginkgo BDD Testing Framework <http://onsi.github.io/ginkgo/>
+// Gomega Matcher Library <http://onsi.github.io/gomega/>
+
+
 package kata_test
 import (
-  ."github.com/onsi/ginkgo/v2"
+  . "github.com/onsi/ginkgo/v2"
   . "github.com/onsi/gomega"
   . "fundamentals/kata"
 )
 
-func dotest(n int, expected bool) {
-     Expect(AmIWilson(n)).To(Equal(expected), "With n = %d", n)
-}
 
 var _ = Describe("Tests", func() {
      It("Sample tests", func() {
-       dotest(0, false)
-       dotest(1, false)
-       dotest(5, true)
-       dotest(8, false)
-       dotest(9, false)
-       dotest(101, false)
-       dotest(147, false)
-       dotest(151, false)
+       Expect(BinToDec("0")).To(Equal(0))
+       Expect(BinToDec("1")).To(Equal(1))
+       Expect(BinToDec("10")).To(Equal(2))
+       Expect(BinToDec("11")).To(Equal(3))
+       Expect(BinToDec("101010")).To(Equal(42))
+       Expect(BinToDec("1001001")).To(Equal(73))
      })
 })
