2880ae6c9c51a52c6bdad4b6b6e0db219d65762e
Jacek programmer <jacek301@gmail.com>
2024-08-04 02:33:37 +0200

    Rotate for a Max  kata solution not mine

--- kata/kata.go
+++ kata/kata.go
@@ -1,5 +1,18 @@
 package kata
 
+import (
+	"strconv"
+)
+
 func MaxRot(n int64) int64 {
-    return 8
+   str := strconv.FormatInt(n, 10)
+   max := n
+   
+   for i := 0; i<len(str)-1 ; i++ {
+     str = str[:i]+str[i+1:]+string(str[i])
+     num, _:= strconv.ParseInt(str, 10, 64)
+     if max < num { max = num }
+   }
+   
+   return max
 }
