2610e78afe9e53201820b493ad18215060a575a1
Jacek programmer <jacek301@gmail.com>
2024-08-04 15:51:29 +0200

    Help Suzuki rake his garden! start

--- kata/kata.go
+++ kata/kata.go
@@ -1,20 +1,6 @@
 package kata
 
-import (
-	"time"
-)
 
-func UnluckyDays(year int) int {
-
-	fridays13InYear := 0
-	for month := time.January; month <= time.December; month++ {
-		thirteenthDay := time.Date(year, month, 13, 0, 0, 0, 0, time.UTC)
-		
-		if thirteenthDay.Weekday() == time.Friday {
-			fridays13InYear++
-		}
-
-	}
-
-	return fridays13InYear
+func RakeGarden(garden string) string {
+  return "gardern"
 }
--- kata/kata_test.go
+++ kata/kata_test.go
@@ -1,14 +1,21 @@
-package kata_test
+// TODO: replace with your own tests (TDD). An example to get you started is included below.
+// Ginkgo BDD Testing Framework <http://onsi.github.io/ginkgo/>
+// Gomega Matcher Library <http://onsi.github.io/gomega/>
 
+package kata_test
 import (
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-    . "fundamentals/kata"
+  . "github.com/onsi/ginkgo/v2"
+  . "github.com/onsi/gomega"
+  . "fundamentals/kata"
 )
 
-var sampleTest = Describe("Sample Tests", func() {
-	It("it should work with the sample tests", func() {
-		Expect(UnluckyDays(2015)).To(Equal(3))
-		Expect(UnluckyDays(1986)).To(Equal(1))
-	})
+func dotest(s, expected string) {
+     Expect(RakeGarden(s)).To(Equal(expected), "With garden = \"%s\"", s)
+}
+
+var _ = Describe("Tests", func() {
+     It("Sample tests", func() {
+       dotest("slug spider rock gravel gravel gravel gravel gravel gravel gravel", "gravel gravel rock gravel gravel gravel gravel gravel gravel gravel")
+       dotest("gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel", "gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel")
+     })
 })
