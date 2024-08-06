3326f27e276aebd6a0f0a093ca86d901841ecb1e
Jacek programmer <jacek301@gmail.com>
2024-08-06 22:36:26 +0200

    Incrementer kata solution

--- kata/kata.go
+++ kata/kata.go
@@ -1,6 +1,11 @@
 package kata
 
-
 func Incrementer(n []int) []int {
-      // your code here
+
+	lastDigitsAfterInrements := []int{}
+	for index, number := range n {
+		lastDigitsAfterInrements = append(lastDigitsAfterInrements, (number+index+1)%10)
+	}
+
+	return lastDigitsAfterInrements
 }
