67371b22597a9a1c776abd61d3bfbb1d4ee58316
Jacek programmer <jacek301@gmail.com>
2024-08-08 16:59:25 +0200

    If you can read this.. kata start

--- kata/kata.go
+++ kata/kata.go
@@ -1,24 +1,7 @@
 package kata
 
-import (
-	"math"
-)
-
-func WallPaper(l, w, h float64) string {
-
-	numbers := []string{"zero", "one", "two", "three", "four", "five", "six", "seven",
-	"eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-	"sixteen", "seventeen", "eighteen", "nineteen", "twenty"}
-	
-	if (l * w * h == 0) {
-		return numbers[0]
-	}
-	
-	wallArea := 2*((w*h) + (l * h))
-	areaOfOneRoll := 0.52 * 10
-
-	more15Percent := 1.15
-	rollRequire := int(math.Ceil((wallArea/areaOfOneRoll) * more15Percent))
-
-	return numbers[rollRequire]
+func ToNato(words string) string {
+// the NATO map[string]string is preloaded for you
+// NATO["A"] == "Alfa", etc
+    return ""
 }
--- kata/kata_test.go
+++ kata/kata_test.go
@@ -1,21 +1,16 @@
 package kata_test
+ 
 import (
   . "github.com/onsi/ginkgo/v2"
   . "github.com/onsi/gomega"
   . "fundamentals/kata"
-  
 )
-
-func dotest(l, w, h float64, exp string) {
-    var ans = WallPaper(l, w, h)
-    Expect(ans).To(Equal(exp))
-}
-
-var _ = Describe("Tests WallPaper", func() {
-
-    It("should handle basic cases", func() {
-        dotest(6.3, 4.5, 3.29, "sixteen")        
-        dotest(6.3, 5.8, 3.13, "seventeen")
-        dotest(0.0, 5.4, 3.33, "zero")
-    })
+ 
+var _ = Describe("Tests using hard-coded strings", func() {
+  It("Should return a correctly translated string", func() {
+     Expect(ToNato("If you can read")).To(Equal("India Foxtrot Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta"))
+     Expect(ToNato("Did not see that coming")).To(Equal("Delta India Delta November Oscar Tango Sierra Echo Echo Tango Hotel Alfa Tango Charlie Oscar Mike India November Golf"))
+     Expect(ToNato("go for it!")).To(Equal("Golf Oscar Foxtrot Oscar Romeo India Tango !"))
+  })
 })
+ 
