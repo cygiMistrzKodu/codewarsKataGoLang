662ce4d79a53973df3c021474b1cfd977a564c4d
Jacek programmer <jacek301@gmail.com>
2024-08-08 17:48:46 +0200

    If you can read this.. kata solution

--- kata/kata.go
+++ kata/kata.go
@@ -1,7 +1,36 @@
 package kata
 
+import (
+	"strings"
+	"unicode"
+)
+
+var NATO = map[string]string{
+	"A": "Alfa", "B": "Bravo", "C": "Charlie", "D": "Delta", "E": "Echo",
+	"F": "Foxtrot", "G": "Golf", "H": "Hotel", "I": "India", "J": "Juliett",
+	"K": "Kilo", "L": "Lima", "M": "Mike", "N": "November", "O": "Oscar",
+	"P": "Papa", "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango",
+	"U": "Uniform", "V": "Victor", "W": "Whiskey", "X": "X-ray", "Y": "Yankee",
+	"Z": "Zulu",
+}
+
 func ToNato(words string) string {
-// the NATO map[string]string is preloaded for you
-// NATO["A"] == "Alfa", etc
-    return ""
+
+	natoLetters := []string{}
+	for _, letter := range strings.Split(words, "") {
+
+		sign := strings.ToUpper(strings.TrimSpace(letter))
+
+		if len(sign) > 0 {
+			if unicode.IsLetter(rune(sign[0])) {
+				natoLetters = append(natoLetters, NATO[sign])
+			} else {
+				natoLetters = append(natoLetters, sign)
+			}
+
+		}
+
+	}
+
+	return strings.Join(natoLetters, " ")
 }
