dfb41108c24da2ced943a5f9128a39d9d2f32afa
Jacek programmer <jacek301@gmail.com>
2024-08-09 13:34:48 +0200

    Split Strings kata start

--- kata/kata.go
+++ kata/kata.go
@@ -1,36 +1,6 @@
 package kata
 
-import (
-	"strings"
-	"unicode"
-)
+func Solution(str string) []string {
 
-var NATO = map[string]string{
-	"A": "Alfa", "B": "Bravo", "C": "Charlie", "D": "Delta", "E": "Echo",
-	"F": "Foxtrot", "G": "Golf", "H": "Hotel", "I": "India", "J": "Juliett",
-	"K": "Kilo", "L": "Lima", "M": "Mike", "N": "November", "O": "Oscar",
-	"P": "Papa", "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango",
-	"U": "Uniform", "V": "Victor", "W": "Whiskey", "X": "X-ray", "Y": "Yankee",
-	"Z": "Zulu",
-}
-
-func ToNato(words string) string {
-
-	natoLetters := []string{}
-	for _, letter := range strings.Split(words, "") {
-
-		sign := strings.ToUpper(strings.TrimSpace(letter))
-
-		if len(sign) > 0 {
-			if unicode.IsLetter(rune(sign[0])) {
-				natoLetters = append(natoLetters, NATO[sign])
-			} else {
-				natoLetters = append(natoLetters, sign)
-			}
-
-		}
-
-	}
-
-	return strings.Join(natoLetters, " ")
+	return []string{"d"}
 }
--- kata/kata_test.go
+++ kata/kata_test.go
@@ -1,16 +1,13 @@
 package kata_test
- 
 import (
   . "github.com/onsi/ginkgo/v2"
   . "github.com/onsi/gomega"
   . "fundamentals/kata"
 )
- 
-var _ = Describe("Tests using hard-coded strings", func() {
-  It("Should return a correctly translated string", func() {
-     Expect(ToNato("If you can read")).To(Equal("India Foxtrot Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta"))
-     Expect(ToNato("Did not see that coming")).To(Equal("Delta India Delta November Oscar Tango Sierra Echo Echo Tango Hotel Alfa Tango Charlie Oscar Mike India November Golf"))
-     Expect(ToNato("go for it!")).To(Equal("Golf Oscar Foxtrot Oscar Romeo India Tango !"))
-  })
+var _ = Describe("Split Strings", func() {
+    It("Basic tests", func() {
+        Expect(Solution("abc")).To(Equal([]string{"ab", "c_"}))
+        Expect(Solution("dawsd")).To(Equal([]string{"da", "ws", "d_"}))
+        Expect(Solution("awsaws")).To(Equal([]string{"aw", "sa", "ws"}))
+    })
 })
- 
