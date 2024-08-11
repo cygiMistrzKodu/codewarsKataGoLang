dfe684ebd1aea52eb4ace591fe0da4c470c147d4
Jacek programmer <jacek301@gmail.com>
2024-08-12 01:29:00 +0200

    Basics 08: Find next higher number with same Bits (1's) kata not mine  solution 2

--- kata/kata.go
+++ kata/kata.go
@@ -2,9 +2,28 @@ package kata
 
 
 
-func NextHigher(n int) int {
-	r := n & -n
-  p := n + r
-  q := (n ^ p) / (4 * r)
-  return p | q
+func NextHigher(x int) int {
+	// right most set bit
+	rightOne := x & -x
+
+	// reset the pattern and set next higher bit
+	// left part of x will be here
+	nextHigherOneBit := x + rightOne
+
+	// nextHigherOneBit is now part [D] of the above explanation.
+
+	// isolate the pattern
+	rightOnesPattern := x ^ nextHigherOneBit
+
+	// right adjust pattern
+	rightOnesPattern = (rightOnesPattern) / rightOne
+
+	// correction factor
+	rightOnesPattern >>= 2
+
+	// rightOnesPattern is now part [A] of the above explanation.
+
+	// integrate new pattern (Add [D] and [A])
+	next := nextHigherOneBit | rightOnesPattern
+	return next
 }
