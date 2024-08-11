ed63ced6f80e45b8cc2fd0337cb59a6626b7cb8a
Jacek programmer <jacek301@gmail.com>
2024-08-12 01:25:49 +0200

    Basics 08: Find next higher number with same Bits (1's) kata start and solution not mine

--- kata/kata.go
+++ kata/kata.go
@@ -1,19 +1,10 @@
 package kata
 
-import (
-	"regexp"
-)
 
-func Solution(str string) []string {
 
-	regexPattern := regexp.MustCompile(`..|.`)
-	result := regexPattern.FindAllString(str, -1)
-
-	for index, text := range result {
-		if len(text) < 2 {
-			result[index] = text + "_"
-		}
-
-	}
-	return result
+func NextHigher(n int) int {
+	r := n & -n
+  p := n + r
+  q := (n ^ p) / (4 * r)
+  return p | q
 }
--- kata/kata_test.go
+++ kata/kata_test.go
@@ -1,13 +1,24 @@
 package kata_test
+
 import (
   . "github.com/onsi/ginkgo/v2"
   . "github.com/onsi/gomega"
   . "fundamentals/kata"
+  "fmt"
 )
-var _ = Describe("Split Strings", func() {
-    It("Basic tests", func() {
-        // Expect(Solution("abc")).To(Equal([]string{"ab", "c_"}))
-        Expect(Solution("dawsd")).To(Equal([]string{"da", "ws", "d_"}))
-        Expect(Solution("awsaws")).To(Equal([]string{"aw", "sa", "ws"}))
-    })
+
+func doTest(n int, expected int) {
+  It(fmt.Sprintf("n = %d", n), func() {
+    Expect(NextHigher(n)).To(Equal(expected))
+  })
+}
+
+var _ = Describe("Testing NextHigher", func() {
+  Context("Basic tests", func() {
+    doTest(1022, 1279)
+    doTest(128, 256)
+    doTest(1, 2)
+    doTest(127, 191)
+    doTest(1253343, 1253359)
+  })
 })
