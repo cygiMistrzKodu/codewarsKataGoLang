8a1bbe9bc1164178d46ae01bb65376b30e01e6f6
Jacek programmer <jacek301@gmail.com>
2024-08-12 01:32:55 +0200

    Basics 08: Find next higher number with same Bits (1's) kata not mine  solution 4 more of using string conversion.

--- kata/kata.go
+++ kata/kata.go
@@ -1,11 +1,30 @@
 package kata
 
+import (
+	"strconv"
+	"strings"
+)
 
+func reset(s string) string {
+	m := 0
+	a := []rune(s)
+	for i := len(a) - 1; i >= 0; i-- {
+		if a[i] == '0' {
+			a[i] = '1'
+			m += 1
+		}
+	}
+	return strings.Repeat("0", m) + string(a)[m:]
+}
 
 func NextHigher(n int) int {
-	firstOneMask := n & (-n)
-	result := firstOneMask + n
-	ones := result ^ n
-	ones = (ones >> 2) / firstOneMask   
-	return (result | ones )
+	s := strconv.FormatInt(int64(n), 2)
+	i := strings.LastIndex(s, "01")
+	if i != -1 {
+		s = s[0:i] + "10" + reset(s[i+2:])
+	} else {
+		s = "10" + reset(s[1:])
+	}
+	r, _ := strconv.ParseInt(s, 2, 32)
+	return int(r)
 }
