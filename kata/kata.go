cd6b557bd455c2fa192e88796930834ddd71bae0
Jacek programmer <jacek301@gmail.com>
2024-08-17 22:58:00 +0200

    Amidakuji kata another solution not mine

--- kata/kata.go
+++ kata/kata.go
@@ -2,57 +2,16 @@ package kata
 
 func Amidakuji(ar []string) []int {
 
-	players := make([]int, len(ar[0])+1)
-	for playerNumber := range players {
-		players[playerNumber] = playerNumber
-		playerNumber++
+	nums := make([]int, len(ar[0])+1)
+	for i := range nums {
+		nums[i] = i
 	}
-
-	playersFinalPostions := make([]int, len(ar[0])+1)
-
-	var leftSide rune
-	var rightSide rune
-
-	for playerPostion, playerNumber := range players {
-
-		playerFinalPostion := playerPostion
-
-		for _, ladderFloor := range ar {
-
-			if playerFinalPostion > 0 {
-
-				leftSide = rune(ladderFloor[playerFinalPostion-1])
-			} else {
-				leftSide = '0'
+	for _, row := range ar {
+		for i, s := range row {
+			if s == '1' {
+				nums[i], nums[i+1] = nums[i+1], nums[i]
 			}
-
-			if playerFinalPostion < len(ladderFloor) {
-
-				rightSide = rune(ladderFloor[playerFinalPostion])
-			} else {
-				rightSide = '0'
-			}
-
-			if leftSide != 0 {
-
-				if leftSide == '1' {
-					playerFinalPostion--
-				}
-
-			}
-
-			if rightSide != 0 {
-
-				if rightSide == '1' {
-					playerFinalPostion++
-
-				}
-			}
-
 		}
-
-		playersFinalPostions[playerFinalPostion] = playerNumber
 	}
-
-	return playersFinalPostions
+	return nums
 }
