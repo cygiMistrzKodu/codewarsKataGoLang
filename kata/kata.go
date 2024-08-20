47fbe9b6d2ccdfeaffdfb37770c397824be162c9
Jacek programmer <jacek301@gmail.com>
2024-08-20 13:43:26 +0200

    The Supermarket Queue kata solution using slices

--- kata/kata.go
+++ kata/kata.go
@@ -1,7 +1,28 @@
 package kata
 
+import "slices"
 
 func QueueTime(customers []int, n int) int {
-      // your code here
-			return 0
+
+	if len(customers) <= 0 {
+		return 0
+	}
+
+	queues := []int{}
+
+	for position, customerTime := range customers {
+
+		if position < n {
+			queues = append(queues, customerTime)
+		} else {
+
+			lowestSumTime := slices.Min(queues)
+			lowestSumIndex := slices.Index(queues, lowestSumTime)
+			queues[lowestSumIndex] = lowestSumTime + customerTime
+
+		}
+
+	}
+
+	return slices.Max(queues)
 }
--- kata/kata_test.go
+++ kata/kata_test.go
@@ -15,8 +15,8 @@ var _ = Describe("Tests", func() {
      It("Sample tests", func() {
        Expect(QueueTime([]int{}, 1)).To(Equal(0))
        Expect(QueueTime([]int{1,2,3,4}, 1)).To(Equal(10))
-       Expect(QueueTime([]int{2,2,3,3,4,4}, 2)).To(Equal(9))
        Expect(QueueTime([]int{1,2,3,4,5}, 100)).To(Equal(5))
+       Expect(QueueTime([]int{2,2,3,3,4,4}, 2)).To(Equal(9))  // teraz ten przypadek 
      })
 })
 
