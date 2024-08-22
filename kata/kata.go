f84f900a401e232878e94d0fcd2cad2e158cdd7b
Jacek programmer <jacek301@gmail.com>
2024-08-22 23:18:50 +0200

    The Supermarket Queue kata solution with structure and more hand coding

--- kata/kata.go
+++ kata/kata.go
@@ -1,6 +1,17 @@
 package kata
 
-import "slices"
+type NumberIndex struct {
+	number int
+	index  int
+}
+
+func (numberIndex NumberIndex) GetIndex() int {
+	return numberIndex.index
+}
+
+func (numberIndex NumberIndex) GetValue() int {
+	return numberIndex.number
+}
 
 func QueueTime(customers []int, n int) int {
 
@@ -16,13 +27,41 @@ func QueueTime(customers []int, n int) int {
 			queues = append(queues, customerTime)
 		} else {
 
-			lowestSumTime := slices.Min(queues)
-			lowestSumIndex := slices.Index(queues, lowestSumTime)
-			queues[lowestSumIndex] = lowestSumTime + customerTime
+			lowestSumElement := LowestSum(queues)
+			lowestSumTime := lowestSumElement.GetValue()
+			indexOfMinSum := lowestSumElement.GetIndex()
+
+			queues[indexOfMinSum] = lowestSumTime + customerTime
 
 		}
 
 	}
 
-	return slices.Max(queues)
+	return Max(queues)
+}
+
+func LowestSum(queues []int) NumberIndex {
+	lowestSum := queues[0]
+	position := 0
+	for index, sum := range queues {
+		if lowestSum > sum {
+			lowestSum = sum
+			position = index
+		}
+	}
+
+	return NumberIndex{index: position, number: lowestSum}
+
+}
+
+func Max(queues []int) int {
+	max := queues[0]
+
+	for _, time := range queues {
+		if time > max {
+			max = time
+		}
+	}
+
+	return max
 }
--- kata/kata_test.go
+++ kata/kata_test.go
@@ -3,20 +3,19 @@
 // Gomega Matcher Library <http://onsi.github.io/gomega/>
 
 package kata_test
+
 import (
-  . "github.com/onsi/ginkgo/v2"
-  . "github.com/onsi/gomega"
-  . "fundamentals/kata"
+	. "fundamentals/kata"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-
-
 var _ = Describe("Tests", func() {
-     It("Sample tests", func() {
-       Expect(QueueTime([]int{}, 1)).To(Equal(0))
-       Expect(QueueTime([]int{1,2,3,4}, 1)).To(Equal(10))
-       Expect(QueueTime([]int{1,2,3,4,5}, 100)).To(Equal(5))
-       Expect(QueueTime([]int{2,2,3,3,4,4}, 2)).To(Equal(9))  // teraz ten przypadek 
-     })
+	It("Sample tests", func() {
+		Expect(QueueTime([]int{}, 1)).To(Equal(0))
+		Expect(QueueTime([]int{1, 2, 3, 4}, 1)).To(Equal(10))
+		Expect(QueueTime([]int{1, 2, 3, 4, 5}, 100)).To(Equal(5))
+		Expect(QueueTime([]int{2, 2, 3, 3, 4, 4}, 2)).To(Equal(9))
+		Expect(QueueTime([]int{43, 30, 27, 38, 14, 14, 5, 2, 29, 36, 39, 38}, 6)).To(Equal(68))
+	})
 })
-
