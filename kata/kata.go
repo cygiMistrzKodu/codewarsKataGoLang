741183446d847ba53d3ddfce192f4a280af27f17
Jacek programmer <jacek301@gmail.com>
2024-08-24 00:43:04 +0200

    What century is it? kata start

--- kata/kata.go
+++ kata/kata.go
@@ -1,67 +1,7 @@
 package kata
 
-type NumberIndex struct {
-	number int
-	index  int
+
+func WhatCentury(year string) string {
+  return "0th"
 }
 
-func (numberIndex NumberIndex) GetIndex() int {
-	return numberIndex.index
-}
-
-func (numberIndex NumberIndex) GetValue() int {
-	return numberIndex.number
-}
-
-func QueueTime(customers []int, n int) int {
-
-	if len(customers) <= 0 {
-		return 0
-	}
-
-	queues := []int{}
-
-	for position, customerTime := range customers {
-
-		if position < n {
-			queues = append(queues, customerTime)
-		} else {
-
-			lowestSumElement := LowestSum(queues)
-			lowestSumTime := lowestSumElement.GetValue()
-			indexOfMinSum := lowestSumElement.GetIndex()
-
-			queues[indexOfMinSum] = lowestSumTime + customerTime
-
-		}
-
-	}
-
-	return Max(queues)
-}
-
-func LowestSum(queues []int) NumberIndex {
-	lowestSum := queues[0]
-	position := 0
-	for index, sum := range queues {
-		if lowestSum > sum {
-			lowestSum = sum
-			position = index
-		}
-	}
-
-	return NumberIndex{index: position, number: lowestSum}
-
-}
-
-func Max(queues []int) int {
-	max := queues[0]
-
-	for _, time := range queues {
-		if time > max {
-			max = time
-		}
-	}
-
-	return max
-}
--- kata/kata_test.go
+++ kata/kata_test.go
@@ -3,19 +3,22 @@
 // Gomega Matcher Library <http://onsi.github.io/gomega/>
 
 package kata_test
-
 import (
-	. "fundamentals/kata"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+  . "github.com/onsi/ginkgo/v2"
+  . "github.com/onsi/gomega"
+  . "fundamentals/kata"
 )
 
+func dotest(y, expected string) {
+     Expect(WhatCentury(y)).To(Equal(expected), "With year = \"%s\"", y)
+}
+
 var _ = Describe("Tests", func() {
-	It("Sample tests", func() {
-		Expect(QueueTime([]int{}, 1)).To(Equal(0))
-		Expect(QueueTime([]int{1, 2, 3, 4}, 1)).To(Equal(10))
-		Expect(QueueTime([]int{1, 2, 3, 4, 5}, 100)).To(Equal(5))
-		Expect(QueueTime([]int{2, 2, 3, 3, 4, 4}, 2)).To(Equal(9))
-		Expect(QueueTime([]int{43, 30, 27, 38, 14, 14, 5, 2, 29, 36, 39, 38}, 6)).To(Equal(68))
-	})
+     It("Sample tests", func() {
+       dotest("2011", "21st")
+       dotest("2154", "22nd")
+       dotest("2259", "23rd")
+       dotest("1234", "13th")
+       dotest("1023", "11th")
+     })
 })
