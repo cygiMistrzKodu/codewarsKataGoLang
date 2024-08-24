396d57c4373c174234f8657de6584b0b6e1a7687
Jacek programmer <jacek301@gmail.com>
2024-08-24 18:01:05 +0200

    What century is it? kata solution

--- kata/kata.go
+++ kata/kata.go
@@ -1,7 +1,39 @@
 package kata
 
+import "strconv"
 
 func WhatCentury(year string) string {
-  return "0th"
-}
 
+	endingsPostfix := map[int]string{
+		1: "st",
+		2: "nd",
+		3: "rd",
+		4: "th",
+		5: "th",
+		6: "th",
+		7: "th",
+		8: "th",
+		9: "th",
+		0: "th",
+	}
+
+	yearNumber, _ := strconv.ParseInt(year, 10, 64)
+
+	if yearNumber%1000 == 0 || yearNumber%100 == 0 {
+		centaury := (yearNumber / 100)
+
+		if centaury >= 11 && centaury <= 13 {
+			return strconv.Itoa(int(centaury)) + "th"
+		}
+		return strconv.Itoa(int(centaury)) + endingsPostfix[int(centaury%10)]
+
+	}
+
+	centaury := (yearNumber / 100) + 1
+
+	if centaury >= 11 && centaury <= 13 {
+		return strconv.Itoa(int(centaury)) + "th"
+	}
+
+	return strconv.Itoa(int(centaury)) + endingsPostfix[int(centaury%10)]
+}
--- kata/kata_test.go
+++ kata/kata_test.go
@@ -3,22 +3,27 @@
 // Gomega Matcher Library <http://onsi.github.io/gomega/>
 
 package kata_test
+
 import (
-  . "github.com/onsi/ginkgo/v2"
-  . "github.com/onsi/gomega"
-  . "fundamentals/kata"
+	. "fundamentals/kata"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 func dotest(y, expected string) {
-     Expect(WhatCentury(y)).To(Equal(expected), "With year = \"%s\"", y)
+	Expect(WhatCentury(y)).To(Equal(expected), "With year = \"%s\"", y)
 }
 
 var _ = Describe("Tests", func() {
-     It("Sample tests", func() {
-       dotest("2011", "21st")
-       dotest("2154", "22nd")
-       dotest("2259", "23rd")
-       dotest("1234", "13th")
-       dotest("1023", "11th")
-     })
+	It("Sample tests", func() {
+		dotest("2000", "20th")
+		dotest("2011", "21st")
+		dotest("2154", "22nd")
+		dotest("2259", "23rd")
+		dotest("1234", "13th")
+		dotest("1023", "11th")
+		dotest("4373", "44th")
+		dotest("9400", "94th")
+		dotest("1300", "13th")
+	})
 })
